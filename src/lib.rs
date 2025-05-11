fadf5c3f416ebd879a3672a4908c12cea3933652
kppras <the.kittipongpras@gmail.com>
2025-05-11 16:04:32 +0700

    feat: send message to slack via webhook as attachment includes app_name,error_type,error_message,backtraces

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use backtrace::Backtrace;
+use backtrace::{Backtrace, BacktraceFrame};
 use log::{debug, error, info, warn};
 use thiserror::Error;
 
@@ -14,7 +14,15 @@ pub struct WudClient {
 
 struct WudError {
     source: Box<dyn std::error::Error + Send + Sync>,
+    error_type: String,
     backtrace: Option<Backtrace>,
+}
+
+struct ErrorEvent {
+    app_name: String,
+    error_type: String,
+    error_message: String,
+    backtrace_frames: Vec<BacktraceFrame>,
 }
 
 impl std::fmt::Display for WudError {
@@ -46,6 +54,7 @@ impl WudError {
     {
         Self {
             source: Box::new(error),
+            error_type: std::any::type_name::<E>().to_string(),
             backtrace: Some(Backtrace::new()),
         }
     }
@@ -64,12 +73,123 @@ impl WudClient {
         E: std::error::Error + Send + Sync + 'static,
     {
         let error = WudError::new(error);
-        let error_type = std::any::type_name::<E>().to_string();
-        error!("{error_type}:{error}");
 
-        // TODO : create a new struct calls error_event to store the error_type and error_message, and backtrace
-        // TODO : send the error event to the provider
-        if let Some(backtrace) = error.backtrace {
+        let error_event = self.create_error_event(error);
+
+        error!("{}:{}", error_event.error_type, error_event.error_message);
+
+        self.send_to_slack(error_event).await?;
+
+        Ok(())
+    }
+
+    async fn send_to_slack(
+        &self,
+        error_event: ErrorEvent,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let url = self.config.slack_webhook_url.clone();
+
+        let client = reqwest::Client::new();
+
+        let mut backtrace_text = String::new();
+        for frame in error_event.backtrace_frames.iter() {
+            for symbol in frame.symbols() {
+                if let (Some(filename), Some(lineno), Some(name)) = (
+                    symbol.filename(),
+                    symbol.lineno(),
+                    symbol.name().map(|s| format!("{:#}", s)),
+                ) {
+                    if filename.to_string_lossy().contains("/home")
+                        && !filename.to_string_lossy().contains(".cargo")
+                        && !filename.to_string_lossy().contains(".rustc")
+                        && !filename.to_string_lossy().contains("lib.rs")
+                    {
+                        let file = filename.display().to_string();
+                        let line = lineno;
+                        let func = name.clone();
+                        if !backtrace_text.is_empty() {
+                            backtrace_text.push_str("\n");
+                        }
+                        backtrace_text.push_str(&format!("*{}* in `{}` at `{}`", file, func, line));
+                        debug!(
+                            "Reporting error to Slack: {}",
+                            format!("\n{} in {} at {}", file, func, line)
+                        );
+                    }
+                }
+            }
+        }
+
+        let mut payload = serde_json::json!({
+            "attachments": [
+                {
+                    "color": "#FF0000",
+                    "blocks": [
+                        {
+                            "type": "header",
+                            "text": {
+                                "type": "plain_text",
+                                "text": format!(":warning: WUD Report | {}", error_event.app_name),
+                                "emoji": true,
+                            },
+                        },
+                        {
+                            "type": "section",
+                            "fields": [
+                                {
+                                    "type": "mrkdwn",
+                                    "text": format!(
+                                        "*Error Type:*\n{}",
+                                        error_event.error_type
+                                    ),
+                                },
+                                {
+                                    "type": "mrkdwn",
+                                    "text": format!(
+                                        "*Error Message:*\n{}",
+                                        error_event.error_message
+                                    ),
+                                },
+                            ],
+                        },
+                    ]
+                }
+            ],
+        });
+
+        if !backtrace_text.is_empty() {
+            payload["attachments"][0]["blocks"]
+                .as_array_mut()
+                .unwrap()
+                .push(serde_json::json!({
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": format!("*Backtrace:*\n```{}```", backtrace_text),
+                    }
+                }));
+        }
+
+        let resp = client.post(&url).json(&payload).send().await?;
+        debug!("Response: {}", resp.text().await?);
+
+        Ok(())
+    }
+
+    fn create_error_event(&self, error: WudError) -> ErrorEvent {
+        let backtrace_frames = self.backtrace_to_backtrace_frames(&error);
+
+        ErrorEvent {
+            app_name: self.config.app_name.clone(),
+            error_type: error.error_type.clone(),
+            error_message: error.source.to_string(),
+            backtrace_frames,
+        }
+    }
+
+    fn backtrace_to_backtrace_frames(&self, error: &WudError) -> Vec<BacktraceFrame> {
+        let mut bt = Vec::new();
+        if let Some(backtrace) = &error.backtrace {
             for frame in backtrace.frames().iter().take(10) {
                 for symbol in frame.symbols() {
                     if let (Some(filename), Some(lineno), Some(name)) = (
@@ -85,12 +205,13 @@ impl WudClient {
                             let file = filename.display().to_string();
                             let line = lineno;
                             let func = name.clone();
-                            error!("{file} in {func} at {line}",);
+
+                            bt.push(frame.clone());
                         }
                     }
                 }
             }
         }
-        Ok(())
+        bt
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
+use std::env;
+
 use anyhow::Result;
 use payment::process_payment;
 use thiserror::Error;
 use wud::{Config, WudClient};
+use dotenv::dotenv;
 
 mod payment;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
     env_logger::init();
 
+    let slack_webhook_url = env::var("SLACK_WEBHOOK_URL")?;
+
     let wud_config = Config {
-        app_name: "wud".to_string(),
-        slack_webhook_url:
-            "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX"
-                .to_string(),
+        app_name: "Test App".to_string(),
+        slack_webhook_url,
     };
 
     let wud_client = WudClient::new(wud_config);
